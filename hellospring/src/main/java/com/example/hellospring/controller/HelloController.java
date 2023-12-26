25e6da83a9e6c6128d13afb7a2f28aefe8c43ed1
kyungmi kang <carami@nate.com>
2023-12-26 10:55:46 +0900

    add exam

---
+++ hellospring/src/main/java/com/example/hellospring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello spring boot!!!";
+    }
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+//    @ResponseBody
+    public String hi(){
+        return "hi~~~carami!!!";
+    }
+}
