48ba82299b8496d311fb9990cd1abf6f01691a75
kyungmi kang <carami@nate.com>
2023-12-27 11:59:52 +0900

    add iocexam

--- shop/src/main/java/com/example/shop/controller/HelloController.java
+++ shop/src/main/java/com/example/shop/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -12,4 +15,17 @@ public class HelloController {
     public String hi(){
         return "hi~~";
     }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public List<String> hello(){
+        List<String> names = new ArrayList<>();
+        names.add("kang");
+        names.add("kim");
+        names.add("hong");
+        names.add("lee");
+        return names;
+
+    }
+
 }
