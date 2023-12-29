510bdc36277eb3d2451a095269306f6493fe4a70
kyungmi kang <carami@nate.com>
2023-12-29 11:18:25 +0900

    update todo

--- todo/src/main/java/com/example/todo/TodoApplication.java
+++ todo/src/main/java/com/example/todo/TodoApplication.java
@@ -1,13 +1,29 @@
 package com.example.todo;
 
+import com.example.todo.domain.Todo;
+import com.example.todo.repository.TodoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Autowired
+	TodoDao todoDao;
+	@Override
+	public void run(String... args) throws Exception {
+		List<Todo> todos =  todoDao.findAll();
+		System.out.println(todos.size());
+		for(Todo todo : todos){
+			System.out.println(todo);
+		}
+	}
 }
--- todo/src/main/java/com/example/todo/repository/TodoDao.java
+++ todo/src/main/java/com/example/todo/repository/TodoDao.java
@@ -1,9 +1,52 @@
 package com.example.todo.repository;
 
+import com.example.todo.domain.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class TodoDao {
     //JDBC
     //1.접속   -- Connection
     //2. 쿼리생성   -- Statement - PreParedStatement, Call~~
     //3. 실행  -     결과  ResultSet
     //4. 결과얻기..
+
+    public List<Todo> findAll(){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        List<Todo> todos = new ArrayList<>();
+
+        try{
+            //1.접속
+            conn = DBUtil.getConnection();
+            String sql = "select id, todo, done from todos order by id desc";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while(rs.next()){
+                Todo todo = new Todo();
+                todo.setId(rs.getLong("id"));
+                todo.setContent(rs.getString("todo"));
+                todo.setDone(rs.getBoolean("done"));
+
+                todos.add(todo);
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            DBUtil.close(conn,ps,rs);
+        }
+
+        return todos;
+    }
 }
